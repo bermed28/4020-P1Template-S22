c9c80bfe2c6112dc6765575e5209e58c577cdac2
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2022-02-25 20:51:00 -0400

    Final Commit

--- src/Main/Ballot.java
+++ src/Main/Ballot.java
@@ -9,26 +9,6 @@ package Main;
  * @version: 2.0
  * @since: 2022-02-22
  */
-public class Ballot implements BaseBallot{
-
-	@Override
-	public int getBallotNum() {
-		return 0;
-	}
-
-	@Override
-	public int getRankByCandidate(int candidateID) {
-		return 0;
-	}
-
-	@Override
-	public int getCandidateByRank(int rank) {
-		return 0;
-	}
-
-	@Override
-	public boolean eliminate(int candidateId) {
-		return false;
-	}
+public class Ballot /*TODO IMPLEMENT BaseBallot*/{
 
 }
--- src/Main/Election.java
+++ src/Main/Election.java
@@ -13,17 +13,7 @@ package Main;
 public class Election {
 	
 	public static void main(String[] args) {
-		/*TODO CALL THE FUNCTION THAT BEGINS YOUR PROGRAM
-		 * 
-		 * NOTE: YOUR INITAL FUNCTION MUST PASS AS PARAMETER THE LOCATION OF 
-		 * THE INPUT FILES USING THE args PARAMETER OF main()...
-		 * 
-		 * 
-		 * args[0] has to contain the path to ballots.csv
-		 * args[1] has to contain the path to candidates.csv
-		 * 
-		 * YOU MUST USE THESE PARAMETERS (args[0], args[1]) IN ANY AUXILIARY FUNCTION YOU MAY USE
-		 **/
+		/*TODO CALL THE FUNCTION THAT BEGINS YOUR PROGRAM*/
 	}
 	
 }
